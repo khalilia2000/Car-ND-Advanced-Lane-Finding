da0021d3d58ebbfb8dd3b2d9597b36dbf19b2d1c
ali khalili <ali.khalili@gmail.com>
2017-01-09 22:21:10 -0800

    feat: add helper functions for sobel gradient

--- calibrate.py
+++ calibrate.py
@@ -10,10 +10,10 @@ import cv2
 import glob
 import matplotlib.pyplot as plt
 
-cal_dir = "C:/Users/ali.khalili/Desktop/Car-ND/CarND-P4-Advanced-Lane-Lines/camera_cal/"
+cal_dir = "C:/Udacity Courses/Car-ND-Udacity/P4-Advanced-Lane-Lines/camera_cal/"
 
 
-def calibrate_camera(cal_path, nx, ny, save_with_corners=True, save_undistort=True):
+def calibrate_camera_from_path(cal_path, nx, ny, save_with_corners=True, save_undistort=True):
     """
     returns camera matrix and distortion matrix based on all the chessboard pictures located in the path
     cal_path: is the path to the calibration files. All calibration files are assumed to have 
@@ -73,4 +73,90 @@ def calibrate_camera(cal_path, nx, ny, save_with_corners=True, save_undistort=Tr
             cv2.imwrite(cal_path+write_name,dst)
         
     return mtx, dist
-      
+   
+
+def abs_sobel_thresh(img, orient='x', sobel_kernel=3, thresh=(0, 255)):
+    '''
+    Calculates directional gradient, and applies threshold. returns a binary image
+    img: original image
+    orient: either 'x' or 'y', and determines the direction of sobel operator
+    sobel_kernel: the size of sobel_kernel
+    thresh: threshold for binary image creation
+    '''
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    if orient == 'x':
+        sobelg = cv2.Sobel(gray, cv2.CV_64F, 1, 0)
+    else:
+        sobelg = cv2.Sobel(gray, cv2.CV_64F, 0, 1)
+    abs_sobel = np.absolute(sobelg)
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    
+    sbinary = np.zeros_like(scaled_sobel)
+    sbinary[(scaled_sobel >= thresh[0]) & (scaled_sobel <= thresh[1])] = 1
+    
+    binary_output = sbinary
+    
+    return binary_output
+    
+    
+
+def mag_thresh(img, sobel_kernel=3, mag_thresh=(0, 255)):
+    '''
+    Calculates the magnitude of sobel gradient, and applies threshold. returns a binary image
+    img: original image
+    sobel_kernel: the size of sobel_kernel
+    mag_thresh: threshold for binary image creation
+    '''
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobelg = ((sobelx**2+sobely**2)**0.5)
+    abs_sobel = np.absolute(sobelg)
+    scaled_sobel = np.uint8(255*abs_sobel/np.max(abs_sobel))
+    
+    sbinary = np.zeros_like(scaled_sobel)
+    sbinary[(scaled_sobel >= mag_thresh[0]) & (scaled_sobel <= mag_thresh[1])] = 1
+    
+    binary_output = sbinary
+    
+    #binary_output = np.copy(img) # Remove this line
+    return binary_output
+    
+    
+
+def dir_threshold(img, sobel_kernel=3, thresh=(0, np.pi/2)):
+    '''
+    Calculates the direction of sobel gradient, and applies threshold. returns a binary image
+    img: original image
+    sobel_kernel: the size of sobel_kernel
+    mag_thresh: threshold for binary image creation
+    '''
+
+    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    sobelx = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
+    sobely = cv2.Sobel(gray, cv2.CV_64F, 0, 1, ksize=sobel_kernel)
+    sobelg = ((sobelx**2+sobely**2)**0.5)
+    abs_sobelx = np.absolute(sobelx)
+    abs_sobely = np.absolute(sobely)
+    abs_sobelg = np.absolute(sobelg)
+    
+    dir_sobel = np.arctan2(abs_sobely, abs_sobelx)
+    scaled_sobel = np.uint8(255*abs_sobelg/np.max(abs_sobelg))
+    
+    sbinary = np.zeros_like(scaled_sobel)
+    sbinary[(dir_sobel >= thresh[0]) & (dir_sobel <= thresh[1])] = 1
+    
+    binary_output = sbinary
+    
+    #binary_output = np.copy(img) # Remove this line
+    return binary_output
+
+
+
+def main():
+  pass
+
+
+if __name__=='__main__':
+    main()   
